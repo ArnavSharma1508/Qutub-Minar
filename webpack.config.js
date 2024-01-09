9306a01aacfa62f77a3a43cbcbca23b14898b7ab
unknown <arnavsharma150809@gmail.com>
2024-01-09 22:10:50 +0530

    YOUR_COMMIT_MESSAGE

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './main.js', // your main JavaScript file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
