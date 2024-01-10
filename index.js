64f802a2571b7fdd25e613165c73f03da45ca096
ArnavSharma1508 <156108191+ArnavSharma1508@users.noreply.github.com>
2024-01-10 12:16:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
-			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
